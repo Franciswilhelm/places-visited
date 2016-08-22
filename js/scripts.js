1501881a02a1a4cb6bd42f5427676afb77cada6a
Caleb Paul and Francis Prus <caleb@>
2016-08-22 09:40:35 -0700

    add constructor for new places, and html to support constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,29 @@
 //Business Logic
-var textList = [];
-var ping3 = "ping";
-var pong5 = "pong";
-var pingPong15 = "ping-pong"
-
-function numList(number) {
-  textList.splice(0);
-  textList.push(0);
-  for (var i = 1; i <= number; i++) {
-    textList.push(i);
-    if (i % 15 === 0) {
-      textList.splice(i, 1, pingPong15)
-    } else if (i % 5 === 0) {
-      textList.splice(i, 1, pong5)
-    } else if (i % 3 === 0) {
-      textList.splice(i, 1, ping3)
-    }
-  }
-  return(textList.splice(0, 1));
+function Place(location,landmarks,date,notes) {
+  this.locale = location;
+  this.marks = landmarks;
+  this.calender = date;
+  this.other = notes;
 }
+
 //User Logic
 $(document).ready(function() {
-  $("#pong_form").submit(function(event){
-    event.preventDefault();
-    var startNum = parseInt($("#num_input").val());
-    var output = numList(startNum)
+  $("form#new-trip").submit(function(event) {
 
-    function arrayToList(){
-      var nums = "";
-      for (var i = 0; i < textList.length; i++) {
-        nums += "<li>" + textList[i] + "</li>";
-      }
-      $("#pong_output").empty().append(nums);
-    }
-    arrayToList(textList);
+    event.preventDefault();
+
+    var userLocation = $("input#new-location").val();
+    var userLandmarks = $("input#new-landmarks").val();
+    var userDate = $("input#new-date").val();
+    var userNotes = $("input#new-notes").val();
+
+    var newPlace = new Place(userLocation);
+
+    $("ul#list-places").append("<li><span class='places'>" + newPlace.locale + "</span></li>");
+
+    $("input#new-location").val("");
+    $("input#new-landmarks").val("");
+    $("input#new-date").val("");
+    $("input#new-notes").val("");
   });
 });
